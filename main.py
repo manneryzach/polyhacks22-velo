936e1bc06aa9372ebc78a469524ac568522b52c1
Carl Kristof Tessier <carlkristoftessier@gmailcom>
2022-02-12 11:47:33 -0500

    Added checkboxes

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+'# How bikeable is Montreal?'
+layer_select = st.sidebar.container()
+    
+with layer_select:
+    '## What would you like to see?'
+    bike_layer = st.checkbox('Bike lanes')
+    air_layer = st.checkbox('Air quality')
+    topo_layer = st.checkbox('Topography')
 
 st.map()

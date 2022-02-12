367a3d62713559aebc96de77e8cdf9f89551eb18
GabrielSasseville01 <gabriel.sasseville@umontreal.ca>
2022-02-12 12:06:31 -0500

    Multiple layers

---
+++ Tests/test_layers.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+
+from urllib.error import URLError
+
+'# How bikeable is Montreal?'
+layer_select = st.sidebar.container()
+
+@st.cache
+def from_data_file(filename):
+    url = (
+        "http://raw.githubusercontent.com/streamlit/"
+        "example-data/master/hello/v1/%s" % filename)
+    return pd.read_json(url)
+
+try:
+    ALL_LAYERS = {
+        "Bike lanes": pdk.Layer(
+            "HexagonLayer",
+            data=from_data_file("bike_rental_stats.json"),
+            get_position=["lon", "lat"],
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            extruded=True,
+        ),
+        "Air quality": pdk.Layer(
+            "ScatterplotLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_color=[200, 30, 0, 160],
+            get_radius="[exits]",
+            radius_scale=0.05,
+        ),
+        "Topography": pdk.Layer(
+            "TextLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_text="name",
+            get_color=[0, 0, 0, 200],
+            get_size=15,
+            get_alignment_baseline="'bottom'",
+        ),
+    }
+    st.sidebar.markdown('### Map Layers')
+    selected_layers = [
+        layer for layer_name, layer in ALL_LAYERS.items()
+        if st.sidebar.checkbox(layer_name, True)]
+    if selected_layers:
+        st.pydeck_chart(pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={"latitude": 37.76,
+                                "longitude": -122.4, "zoom": 11, "pitch": 50},
+            layers=selected_layers,
+        ))
+    else:
+        st.error("Please choose at least one layer above.")
+except URLError as e:
+    st.error("""
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """ % e.reason)
